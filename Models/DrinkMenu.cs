ff3dd9efdfdf18733a6dc25245cddab6f4898b63
LeilaLokman <144849535+LeilaLokman@users.noreply.github.com>
2024-08-29 22:15:50 -0400

    Add files via upload

---
+++ Models/DrinkMenu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Bartender_Application.Models
+{
+	public class DrinkMenu
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+	}
+}
+
---
+++ Models/OrderQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Bartender_Application.Models
+{
+    public class OrderQueue
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public int DrinkID { get; set; }
+
+        public virtual DrinkMenu DrinkMenu { get; set; } 
+    }
+}
+
